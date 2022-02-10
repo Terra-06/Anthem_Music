fa29e7046fbb9db3fcb87ef64f5afd993d0b85ed
Terra Smith <t_m@linuxmail.org>
2022-02-09 22:53:39 -0800

    created frontend -- setup states and auth with login & signup

---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="sign-up">
+		<h1>Sign Up</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SignUp',
+};
+</script>
+
+<style>
+.sign-up {
+	color: #7dd526;
+}
+</style>
