74d02144ee9208f59aae5b50c6a2e96aa2dbc53f
Terra Smith <t_m@linuxmail.org>
2022-02-10 21:06:03 -0800

    created a playlist component

---
+++ client/src/components/Playlists.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="playlists">
+		<h1>Playlists</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Playlist',
+};
+</script>
+
+<style>
+.playlists {
+	color: #7dd526;
+}
+</style>
